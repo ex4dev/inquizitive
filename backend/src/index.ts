4830926fca825e205068ca7cb719bfcd69fc533d
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2026-02-14 16:13:00 -0500

    Create stubs for GitHub-related API endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,49 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.post("/api/github/webhook", (c) => {
+  // Handle GitHub webhook payloads
+  return c.text("");
+});
+
+app.get("/api/auth/login", (c) => {
+  const state = ""; // TODO
+  return c.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&state=${state}`,
+  );
+});
+
+app.get("/api/github/install", (c) => {
+  return c.redirect(
+    `https://github.com/apps/${process.env.GITHUB_APP_NAME}/installations/new`,
+  );
+});
+
+app.get("/api/auth/callback", async (c) => {
+  // GitHub OAuth callback
+  // A `code` URL parameter exists with the OAuth code
+  // TODO verify `state`
+  const response = await fetch(
+    `https://github.com/login/oauth/access_token?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}&code=${c.req.query("code")!}`,
+    {
+      method: "POST",
+    },
+  );
+  return c.text("");
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
---
+++ backend/src/octokit.ts
@@ -0,0 +1,30 @@
+import { createAppAuth, createOAuthUserAuth } from "@octokit/auth-app";
+import { Octokit } from "octokit";
+
+const privateKey = Buffer.from(
+  process.env.GITHUB_PRIVATE_KEY!,
+  "base64",
+).toString("utf-8");
+
+export const appOctokit = new Octokit({
+  authStrategy: createAppAuth,
+  auth: {
+    appId: parseInt(process.env.GITHUB_APP_ID!),
+    privateKey,
+    clientId: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  },
+});
+
+export async function getUserOctokit(oauthCode: string) {
+  return (await appOctokit.auth({
+    type: "oauth-user",
+    code: oauthCode,
+    factory: (options) => {
+      return new Octokit({
+        authStrategy: createOAuthUserAuth,
+        auth: options,
+      });
+    },
+  })) as Octokit;
+}
